d718e51c3a48caad6262414cdd4d12d51bd91103
kgostic <kategostic@gmail.com>
2020-01-23 10:09:38 -0600

    Added incubation, fever (f) and risk (g) distributions and probabilities for SARS and MERS to Code_distribution_functions.R

--- Code_distribution_functions.r
+++ Code_distribution_functions.r
@@ -6,22 +6,59 @@
 incubation.function<-function(x,pathogen,type=1){
   #Input x, the time since exposure
   #Output the incubation cdf (type=1) or pdf (type=2)
-
-# Input pathogen-specific distributions (here we give H7N9)
+  
+  # \\\\\\\\\\  H7N9 inputs //////
   if(pathogen == "H7N9"){  
-H7N9.c.nsamp=32
-param.H7N9.fn=c(4.93, 0.63)
-H7N9.fn<-function(x){pgamma(x, param.H7N9.fn[1] ,scale = param.H7N9.fn[2], lower.tail = TRUE,log.p = FALSE)}
-H7N9.fnD<-function(x){dgamma(x, param.H7N9.fn[1] ,scale = param.H7N9.fn[2])}
-
-if(type==1){
-functionpath<-H7N9.fn
-functionpath(x)
-}else{
-functionpath<-H7N9.fnD
-functionpath(x)
-}
-}
+    H7N9.c.nsamp=32
+    param.H7N9.fn=c(4.93, 0.63)
+    H7N9.fn<-function(x){pgamma(x, param.H7N9.fn[1] ,scale = param.H7N9.fn[2], lower.tail = TRUE,log.p = FALSE)}
+    H7N9.fnD<-function(x){dgamma(x, param.H7N9.fn[1] ,scale = param.H7N9.fn[2])}
+    
+    if(type==1){
+      functionpath<-H7N9.fn
+      functionpath(x)
+    }else{
+      functionpath<-H7N9.fnD
+      functionpath(x)
+    }
+  }
+  
+ # \\\\\\  MERS inputs //////
+  if(pathogen == "MERS"){  
+    MERS.c.nsamp=7+23
+    param.MERS.fn=c(log(5.5), log(2.5))
+    MERS.fn<-function(x){plnorm(x, meanlog = param.MERS.fn[1], sdlog = param.MERS.fn[2])}
+    MERS.fnD<-function(x){dlnorm(x, meanlog = param.MERS.fn[1], sdlog =  param.MERS.fn[2])}
+    
+    if(type==1){
+      functionpath<-MERS.fn
+      functionpath(x)
+    }else{
+      functionpath<-MERS.fnD
+      functionpath(x)
+    }
+  }
+  
+  
+  # \\\\\\  SARS inputs //////
+  if(pathogen == "SARS"){  
+    SARS.c.nsamp=57
+    param.SARS.fn=c(2.62, 2.43)
+    SARS.fn<-function(x){pgamma(x, param.SARS.fn[1], scale = param.SARS.fn[2])}
+    SARS.fnD<-function(x){dgamma(x, param.SARS.fn[1], scale = param.SARS.fn[2])}
+    
+    if(type==1){
+      functionpath<-SARS.fn
+      functionpath(x)
+    }else{
+      functionpath<-SARS.fnD
+      functionpath(x)
+    }
+  }
+  
+  
+  
+  
 }
 
 
@@ -31,26 +68,56 @@ functionpath(x)
 fg.distn<-function(type,pathogen){
   #input pathogen name; parameter type (1 = fever, 2 = known risk)
   #output c(value, total sample size)
-       #"value" is calculated as the mean proportion reported across studies reviewed, weighted
-       # by the study-specific sample size
-       #"total sample size" is the sum of sample sizes from all studies considered
-
-# Input pathogen-specific data:
+  #"value" is calculated as the mean proportion reported across studies reviewed, weighted
+  # by the study-specific sample size
+  #"total sample size" is the sum of sample sizes from all studies considered
+  
+  # Input pathogen-specific data:
   if(pathogen == "H7N9"){ 
-  # Fever study sample sizes
-  d1fn=c(85,46,111,46)
-  # Proportions that display fever
-  d1fv=c(0.7,1,1,1)
-
-  # Exposure study sample sizes
-  d1gn=c(123,111,46)
-  # Proportions with known exposure
-  d1gv=c(0.75,0.56,0.78)
+    # Fever study sample sizes
+    d1fn=c(85,46,111,46)
+    # Proportions that display fever
+    d1fv=c(0.7,1,1,1)
+    
+    # Exposure study sample sizes
+    d1gn=c(123,111,46)
+    # Proportions with known exposure
+    d1gv=c(0.75,0.56,0.78)
   }
   
-if(type=="f"){
-c(sum(d1fv*d1fn)/sum(d1fn),sum(d1fn))}else{
-c(sum(d1gv*d1gn)/sum(d1gn),sum(d1gn))}
+  
+  # \\\\\\ MERS ///////
+  if(pathogen == "MERS"){ 
+    # Fever study sample sizes
+    d1fn=c(23)
+    # Proportions that display fever
+    d1fv=c(.87)
+    
+    ######## THIS NEEDS AN UPDATE! 
+    # Exposure study sample sizes
+    d1gn=c(1000)
+    # Proportions with known exposure
+    d1gv=c(0)
+  }
+  
+  # \\\\\\ SARS ///////
+  if(pathogen == "SARS"){ 
+    # Fever study sample sizes
+    d1fn=c(1452)
+    # Proportions that display fever
+    d1fv=c(0.94)
+    
+    ######## THIS NEEDS AN UPDATE! 
+    # Exposure study sample sizes
+    d1gn=c(1192)
+    # Proportions with known exposure
+    d1gv=c(0.29)
+  }
+  
+  
+  if(type=="f"){
+    c(sum(d1fv*d1fn)/sum(d1fn),sum(d1fn))}else{
+      c(sum(d1gv*d1gn)/sum(d1gn),sum(d1gn))}
 }
 
 
@@ -59,13 +126,13 @@ c(sum(d1gv*d1gn)/sum(d1gn),sum(d1gn))}
 # --------------
 fg.binomial<-function(propn,type,pathogen){
   #Input propn = a vector of possible outcomes when sampling individuals from the population
-      # type (1 = fever, 2 = risk awareness (g)), pathogen = "H7N9"
+  # type (1 = fever, 2 = risk awareness (g)), pathogen = "H7N9"
   #Output the binomial density corresponding to each value in "propn"
   #I.e., this function estimates the proability of observing some value of f_hat or
   # g_hat, given that f and g are the true population parameters
   
-aa=fg.distn(type,pathogen) #call the appropriate f, g values from the function above
-dbinom(round(propn*aa[2]),round(aa[2]),aa[1])
+  aa=fg.distn(type,pathogen) #call the appropriate f, g values from the function above
+  dbinom(round(propn*aa[2]),round(aa[2]),aa[1])
 }
 
 
@@ -74,9 +141,9 @@ dbinom(round(propn*aa[2]),round(aa[2]),aa[1])
 # ---------
 pdf.cdf.travel<-function(x,r0,gen.time,type){
   # Input: x - time since exposure, r0 - pathogen R_0 value, gen.time - pathogen generation time,
-       # type - [pdf = 1, cdf = 2]
+  # type - [pdf = 1, cdf = 2]
   # Output: if type = 1, output the density of infection age x among a population of 
-       # exposed individuals attempting travel. If type = 2, output the cumulative density.
+  # exposed individuals attempting travel. If type = 2, output the cumulative density.
   alpha=r0/gen.time
   f01<-function(tt){exp(-alpha*tt)}                  #dI/dt
   f11<-function(tt){(1/alpha)*(1-exp(-alpha*tt))}    #I(t)
